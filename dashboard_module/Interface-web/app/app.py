79c13712fd73a213f2aaecb7c3cf72ae01c8c6c1
suhail1929 <souhailmtarfie@gmail.com>
2024-01-18 13:20:49 +0000

    Site front

---
+++ dashboard_module/Interface-web/app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+ip = "10.11.9.49"
+api_url = "http://"+ip+":5100"
+
+from routes.web import *
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+    
+
+
+
---
+++ dashboard_module/Interface-web/app/routes/web.py
@@ -0,0 +1,35 @@
+from app import app, api_url
+import requests
+from flask import jsonify, render_template, request, flash, redirect, url_for
+from flask_simplelogin import Message, SimpleLogin, get_username, login_required
+
+# Route de la page d'acceuil
+@app.route("/")
+def index():      
+    return render_template('index.html')
+
+# Route pour la création d'un compte
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+# Route pour la récupération des films d'une API externe
+@app.route("/acheter_ticket", methods=['GET', 'POST'])
+def acheter_ticket():
+    return render_template('acheter_ticket.html')
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+# Route de la page administrateur
+@app.route("/administrator")
+def administrator():
+    return render_template("administrator.html")
+
+# mon profils
+
