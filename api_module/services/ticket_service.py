ea1b318e7d95d48943ce8affa84949df8623e1a4
Hugo Bertrand <hugo.bertrand@etudiant.univ-reims.fr>
2024-01-18 13:24:02 +0100

    test

--- api_module/services/ticket_service.py
+++ api_module/services/ticket_service.py
@@ -4,6 +4,26 @@
 # services/ticket_service.py
 from api_module.models.ticket_model import Ticket
 from flask import jsonify
+from qrcode import make
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+class Qrcode:
+    def __init__(self):
+        pass
+
+    def encode_qrcode(self,data):
+        # Récupérer différentes données pour le nom de l'image png
+        img = make(data)
+        type(img)
+        return img
+
+    def decode_qrcode(self,img):
+        data = decode(Image.open('./'+img+'.png'))
+        print("data {}, type {}".format(data[0].data,type(data[0].data)))
+
+        # Requête vers la BDD
+
 
 class TicketService:
 
@@ -26,3 +46,9 @@ class TicketService:
     @staticmethod
     def delete_ticket(ticket_id):
         pass
+
+
+
+# classe = Qrcode()    
+# img = classe.encode_qrcode(data={"nom" : "olivier", "prenom" : "flauzac"})
+# classe.decode_qrcode(img)
