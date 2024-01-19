7c88f14014ef975ce60f0579419d0839f655931c
suhail1929 <souhailmtarfie@gmail.com>
2024-01-19 13:12:56 +0000

    suite test bdd

--- dashboard_module/Interface-web/app/app.py
+++ dashboard_module/Interface-web/app/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+import secrets
+import requests
+from flask import jsonify, render_template, request, flash, redirect, url_for
+from flask_login import login_user, logout_user, current_user, login_required, LoginManager, UserMixin
+from pymongo import MongoClient
+from werkzeug.security import check_password_hash, generate_password_hash
+from bson import ObjectId
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
+secret_key= secrets.token_urlsafe(16)
+app.config['SECRET_KEY'] = secret_key
 ip = "10.11.9.49"
 api_url = "http://"+ip+":5100"
 
 from routes.web import *
+from routes.auth import *
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- dashboard_module/Interface-web/app/routes/web.py
+++ dashboard_module/Interface-web/app/routes/web.py
@@ -1,40 +1,37 @@
-from app import app, api_url
-import requests
-from flask import jsonify, render_template, request, flash, redirect, url_for
-from flask_simplelogin import Message, SimpleLogin, get_username, login_required
-
+from app import *
 # Route de la page d'acceuil
 @app.route("/")
 def index():      
     return render_template('index.html')
-
-# Route pour la création d'un compte
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    return render_template("register.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
 
 @app.route("/acheter_ticket", methods=['GET', 'POST'])
 def acheter_ticket():
     return render_template('acheter_ticket.html')
 
 @app.route("/acheter_abonnement", methods=['GET', 'POST'])
+@login_required
 def acheter_abonnement():
     return render_template('acheter_abonnement.html')
 
 @app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
 # Route de la page administrateur
 @app.route("/administrator")
+@login_required
 def administrator():
     return render_template("administrator.html")
 
 # mon profils
 @app.route("/profil")
+@login_required
 def profil():
     return render_template('profil.html')
+
+# simulation
+@app.route("/simulation")
+@login_required
+def simulation():
+    return render_template('simulation.html')
