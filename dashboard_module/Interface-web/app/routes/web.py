04c110d1948ba2c3d171837374aaea48dc342804
suhail1929 <souhailmtarfie@gmail.com>
2024-02-03 15:45:59 +0000

    dashborad

--- dashboard_module/Interface-web/app/routes/web.py
+++ dashboard_module/Interface-web/app/routes/web.py
@@ -12,43 +12,69 @@ def acheter_ticket():
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    return render_template('dashboard.html')
+    if "user_id" in session:
+        reponse_total_number_tickets = requests.get(api_url + "/statistics/total_number_tickets")
+        reponse_repartition_types_tickets = requests.get(api_url + "/statistics/repartition_types_tickets")
+        reponse_number_expired_tickets = requests.get(api_url + "/statistics/number_expired_tickets")
+        reponse_turnover = requests.get(api_url + "/statistics/turnover")
+        reponse_most_active_users = requests.get(api_url + "/statistics/most_active_users")
+        reponse_peak_usage_times = requests.get(api_url + "/statistics/peak_usage_times")
+        reponse_repartition_types_personnes = requests.get(api_url + "/statistics/repartition_types_personnes")
+        return render_template('dashboard.html', 
+            total_number_tickets=reponse_total_number_tickets.json(),
+            repartition_types_tickets=reponse_repartition_types_tickets.json(),
+            number_expired_tickets=reponse_number_expired_tickets.json(),
+            turnover=reponse_turnover.json(),
+            most_active_users=reponse_most_active_users.json(),
+            peak_usage_times=reponse_peak_usage_times.json(),
+            repartition_types_personnes=reponse_repartition_types_personnes.json()
+        )
+    else:
+            return redirect(url_for('login'))
 
 # Route de la page administrateur
 @app.route("/administrator")
 def administrator():
-     
-    response = requests.get(api_url+"/users/")
-    
-    if response.status_code == 200:
-        users = response.json()
-        return render_template("administrator.html", users=users)
+    if "user_id" in session:
+        response = requests.get(api_url+"/users/")
+        
+        if response.status_code == 200:
+            users = response.json()
+            return render_template("administrator.html", users=users)
+        else:
+            flash("An error occurred. Please try again later.", "danger")
+        return render_template("administrator.html")
     else:
-        flash("An error occurred. Please try again later.", "danger")
-    return render_template("administrator.html")
+        return redirect(url_for('login'))
 
 # mon profils
 @app.route("/profil")
 def profil():
-    response = requests.get(api_url + "/tickets/")
-    if response.status_code in [200, 201]:
-        tickets = response.json()["tickets"]
-        return render_template('profil.html', tickets=tickets)
-    return render_template('profil.html')
+    if "user_id" in session:
+        response = requests.get(api_url + "/tickets/")
+        if response.status_code in [200, 201]:
+            tickets = response.json()["tickets"]
+            return render_template('profil.html', tickets=tickets)
+        return render_template('profil.html')
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/tickets")
 def tickets():
-    response = requests.get(api_url + "/tickets/")
-    if response.status_code in [200, 201]:
-        tickets = response.json()["tickets"]
-        #format date
-        for ticket in tickets:
-            date_created = datetime.fromisoformat(ticket["created_at"])
-            date_expired = datetime.fromisoformat(ticket["expires_at"])
-            ticket["created_at"] = date_created.strftime("%d/%m/%Y %H:%M:%S")
-            ticket["expires_at"] = date_expired.strftime("%d/%m/%Y %H:%M:%S")
-        return render_template('tickets.html', tickets=tickets)
-    return render_template('tickets.html')
+    if "user_id" in session:
+        response = requests.get(api_url + "/tickets/")
+        if response.status_code in [200, 201]:
+            tickets = response.json()["tickets"]
+            #format date
+            for ticket in tickets:
+                date_created = datetime.fromisoformat(ticket["created_at"])
+                date_expired = datetime.fromisoformat(ticket["expires_at"])
+                ticket["created_at"] = date_created.strftime("%d/%m/%Y %H:%M:%S")
+                ticket["expires_at"] = date_expired.strftime("%d/%m/%Y %H:%M:%S")
+            return render_template('tickets.html', tickets=tickets)
+        return render_template('tickets.html')
+    else:
+        return redirect(url_for('login'))
 
 # simulation
 @app.route("/simulation")
