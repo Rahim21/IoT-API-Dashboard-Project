0aead85d246ac4a55f33439c94bce39e3368604f
haya0002 <rahim.hayat@etudiant.univ-reims.fr>
2024-02-04 01:44:16 +0100

    api_module : Enhance User Model with Role in API Module
    Added functionality to include user roles in the API module's model. Additionally, implemented a new route for migrations, including a default migration to add admin users.

--- api_module/utils.py
+++ api_module/utils.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 # -----------------------------------------------------------------------------
 # Auteurs: BERTRAND Hugo & DRIOUCHE Sami & HAYAT Rahim & MTARFI Souhail
 # -----------------------------------------------------------------------------
@@ -7,12 +5,14 @@
 from pymongo import MongoClient
 import socket
 
+ADDR_IP = "10.11.9.49"
+
 def connect_mongodb() -> MongoClient:
     # Essayer de se connecter à MongoDB en utilisant localhost (127.0.0.1)
     try:
-        mongo_client = MongoClient('mongodb://root:example@10.11.9.49:27017') # IP LOCAL
+        mongo_client = MongoClient(f'mongodb://root:example@{ADDR_IP}:27017') # IP LOCAL
         mongo_client.server_info()  # Vérifier la connexion
-        print("Connecté à MongoDB en utilisant localhost (10.11.9.49)")
+        print(f"Connecté à MongoDB en utilisant localhost ({ADDR_IP})")
         return mongo_client
     except Exception as e1:
         print(f"Erreur lors de la connexion à MongoDB en utilisant localhost : {e1}")
@@ -20,13 +20,12 @@ def connect_mongodb() -> MongoClient:
     # Si la connexion à MongoDB en utilisant localhost échoue, essayer avec l'adresse IP de la connexion SSH
     try:
         ssh_ip = socket.gethostbyname(socket.gethostname()) # IP SSH
-        mongo_client = MongoClient(f'mongodb://root:example@10.11.9.49:27017')
+        mongo_client = MongoClient(f'mongodb://root:example@{ADDR_IP}:27017')
         mongo_client.server_info()  # Vérifier la connexion
-        print(f"Connecté à MongoDB en utilisant l'adresse IP de la connexion SSH : 10.11.9.49")
+        print(f"Connecté à MongoDB en utilisant l'adresse IP de la connexion SSH : {ADDR_IP}")
         return mongo_client
     except Exception as e2:
-        print(f"Erreur lors de la connexion à MongoDB en utilisant l'adresse IP de la connexion SSH : 10.11.9.49")
+        print(f"Erreur lors de la connexion à MongoDB en utilisant l'adresse IP de la connexion SSH : {ADDR_IP}")
 
     # Si toutes les tentatives de connexion échouent, retourner None
     return None
->>>>>>> 58943fdd3f70099f802fdbe437ecf2ee632edcb8
